f5a5b3e3acda7b755398de29319f4f5b7fec68bc
Xio <mxbadano@gmail.com>
2025-01-06 14:18:50 -0300

    fx: firebase config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'projects/update/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'tasks/update/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
